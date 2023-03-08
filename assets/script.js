0f168e0d1fa86565d268efac5a0e4de3ef1a6620
Hank Richter <hankrichter97@gmail.com>
2023-03-08 15:57:29 -0500

    Progress added to the function which will change test area color depending on hour of day

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,40 @@
 // Variables
 const currentDay = $("#currentDay")
+const idNumber9 = $("#hour-9")
+const idNumber10 = $("#hour-10")
+const idNumber11 = $("#hour-11")
+const idNumber12 = $("#hour-12")
+const idNumber13 = $("#hour-13")
+const idNumber14 = $("#hour-14")
+const idNumber15 = $("#hour-15")
+const idNumber16 = $("#hour-16")
+const idNumber17 = $("#hour-17")
+// idNumber9 = 9;
+// idNumber10 = 10;
+// idNumber11 = 11;
+// idNumber12 = 12;
+// idNumber13 = 13;
+// idNumber14 = 14;
+// idNumber15 = 15;
+// idNumber16 = 16;
+// idNumber17 = 17;
 
+// $(function () {)
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$( "textarea" ).addClass(function(idNumber9){
+  if(idNumber9 == dayjs().hour()){
+    idNumber9.addClass(".present");
+  }else if(idNumber9 > dayjs().hour()){
+    idNumber9.addClass(".future")
+  }else{
+    idNumber9.addClass(".future")
+  }
+
+  // https://api.jquery.com/addclass/
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -24,12 +54,11 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.✅
 
-});
+};
 
 function displayTime() {
   const rightNow = dayjs().format('MMM DD, YYYY');
   currentDay.text(rightNow);
 }
-
 
 displayTime()
