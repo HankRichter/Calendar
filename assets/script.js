c1a10871697cf994c4f695dd9476ca86964e5d76
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-03-09 23:06:54 -0500

    Cleaned up variables I did not need

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,6 @@
 // Variables
 const currentDay = $("#currentDay");
 const textArea = $("textarea");
-const idNumber9 = $("#hour-9");
-let idNumber10 = $("#hour-10");
-let idNumber11 = $("#hour-11");
-let idNumber12 = $("#hour-12");
-let idNumber13 = $("#hour-13");
-let idNumber14 = $("#hour-14");
-let idNumber15 = $("#hour-15");
-let idNumber16 = $("#hour-16");
-let idNumber17 = $("#hour-17");
-idNumber9a = 9;
-// idNumber10 = 10;
-// idNumber11 = 11;
-// idNumber12 = 12;
-// idNumber13 = 13;
-// idNumber14 = 14;
-// idNumber15 = 15;
-// idNumber16 = 16;
-// idNumber17 = 17;
-
 // $(function () {)
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
@@ -33,8 +14,6 @@ $(function () {
     idNumber9.addClass("past");
   }
 });
-
-// https://api.jquery.com/addclass/
 
 // TODO: Add a listener for click events on the save button. This code should
 // use the id in the containing time-block as a key to save the user input in
