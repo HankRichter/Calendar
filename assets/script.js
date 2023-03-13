cf4600dda152b41870b1a81dd42965edbd6031c2
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-03-12 22:38:52 -0400

    Cleanup

--- assets/script.js
+++ assets/script.js
@@ -68,4 +68,4 @@ console.log(saveBtn);
 // the values of the corresponding textarea elements. HINT: How can the id
 // attribute of each time-block be used to do this?
 //
-// TODO: Add code to display the current date in the header of the page.✅// TODO: Add code to display the current date in the header of the page.✅
+// TODO: Add code to display the current date in the header of the page.✅
